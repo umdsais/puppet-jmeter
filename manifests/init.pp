d38e9bfa920278f5907a2ecea4765d482811ad59
Benjamin Farley <Ben.Farley@capgemini.com>
2016-10-04 13:52:07 +0100

    Added configurable urls

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,8 +11,13 @@ class jmeter (
   $jmeter_plugins_install = false,
   $jmeter_plugins_version = '1.2.1',
   $jmeter_plugins_set     = ['Standard'],
+  $download_url           = 'http://archive.apache.org/dist/jmeter/binaries/',
+  $plugin_url             = 'http://jmeter-plugins.org/downloads/file/',
   $java_version           = $::jmeter::params::java_version,
 ) inherits ::jmeter::params {
+
+  validate_re($download_url, '^((https?|ftps?):\/\/)([\da-z\.-]+)\.?([\da-z\.]{2,6})([\/\w \.\:-]*)*\/?$')
+  validate_re($plugin_url, '^((https?|ftps?):\/\/)([\da-z\.-]+)\.?([\da-z\.]{2,6})([\/\w \.\:-]*)*\/?$')
 
   Exec { path => '/bin:/usr/bin:/usr/sbin' }
 
@@ -21,7 +26,7 @@ class jmeter (
   ensure_resource('package', [$jdk_pkg, 'unzip', 'wget'], {'ensure' => 'present'})
 
   exec { 'download-jmeter':
-    command => "wget -P /root http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${jmeter_version}.tgz",
+    command => "wget -P /root ${download_url}/apache-jmeter-${jmeter_version}.tgz",
     creates => "/root/apache-jmeter-${jmeter_version}.tgz"
   }
 
@@ -34,8 +39,9 @@ class jmeter (
 
   if $jmeter_plugins_install == true {
     jmeter::plugins_install { $jmeter_plugins_set:
-      plugins_version => $jmeter_plugins_version,
-      require         => [Package['wget'], Package['unzip'], Exec['install-jmeter']],
+      plugins_version   => $jmeter_plugins_version,
+      base_download_url => $plugin_url,
+      require           => [Package['wget'], Package['unzip'], Exec['install-jmeter']],
     }
   }
 }
